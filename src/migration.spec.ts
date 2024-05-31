5339eba348bc08a81728c8e1f6ed02aeb169e8fa
Sy Le <lekhasy@outlook.com>
2024-05-31 17:38:41 +0000

    remove nestjs

--- src/migration.spec.ts
+++ src/migration.spec.ts
@@ -1,5 +1,5 @@
 import { MikroORM } from '@mikro-orm/core';
-// import { PostgresMock } from 'pgmock';
+import { PostgresMock } from 'pgmock';
 import { GetMiKroORMOptions } from './mikro-orm.config';
 import { User } from './entities';
 import dotenv from 'dotenv';
@@ -7,9 +7,9 @@ dotenv.config();
 
 describe('pgmock migration with mikro orm', () => {
   it('Should migrate successfully', async () => {
-    // const pgmock = await PostgresMock.create();
-    // const dbConnectionString = await pgmock.listen(0);
-    // process.env.DATABASE_URL = dbConnectionString;
+    const pgmock = await PostgresMock.create();
+    const dbConnectionString = await pgmock.listen(0);
+    process.env.DATABASE_URL = dbConnectionString;
 
     const orm = await MikroORM.init(GetMiKroORMOptions());
     await orm.getMigrator().up();
